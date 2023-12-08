6453f6deb88e934f1b4d3b739ec0134815211cb1
CeciGav <gavia.chavez.cecilia@gmail.com>
2023-12-07 18:08:57 -0600

    Carpetas actualizadas

---
+++ JavaScript/IntroJavaScript/JavaScript/script.js
@@ -0,0 +1,29 @@
+// comentarios de una sola línea
+
+/*comentarios
+multilinea*/
+
+
+//alert ("Hola mundo");
+
+
+//console.log("Este es un mensaje para mi consola")
+
+/*var nombre = prompt("Ingresa tu nombre");
+alert("Hola, " + nombre +" Bienvenido a la ch y la pizza");*/
+
+/*var numero1 = parseInt (prompt("Ingresa el primer numero"));
+var numero2 = parseInt (prompt("Ingresa el segundo numero"));
+
+console.log("suma:" + (numero1 + numero2));
+console.log("resta:" + (numero1 - numero2));
+console.log("multiplicación:" + (numero1 * numero2));
+console.log("división:" + (numero1 / numero2));*/
+
+var edad = prompt("Por favor, ingresa tu edad:");
+
+if (edad >= 18) {
+    alert("Eres mayor de edad. ¡Bienvenido!");
+  } else {
+    alert("Eres menor de edad. Lo sentimos, debes ser mayor de 18 años para continuar.");
+  }
