83284ee35a8581fdf4b1e1f2a8888f8e917eb605
CeciGav <gavia.chavez.cecilia@gmail.com>
2023-12-08 15:36:09 -0600

    tipos de variables

--- JavaScript/IntroJavaScript/JavaScript/script.js
+++ JavaScript/IntroJavaScript/JavaScript/script.js
@@ -20,10 +20,32 @@ console.log("resta:" + (numero1 - numero2));
 console.log("multiplicación:" + (numero1 * numero2));
 console.log("división:" + (numero1 / numero2));*/
 
-var edad = prompt("Por favor, ingresa tu edad:");
+/*var edad = prompt("Por favor, ingresa tu edad:");
 
 if (edad >= 18) {
     alert("Eres mayor de edad. ¡Bienvenido!");
   } else {
     alert("Eres menor de edad. Lo sentimos, debes ser mayor de 18 años para continuar.");
-  }
+  } MI ALERT*/
+
+
+  //DATOS Y VARIABLES
+  //scope es el alcance de nuestras variables
+  
+  /*var, let, const const la diferencia es el alcance:
+  VAR tiene un alcance global
+  LET tiene un alcance local
+  CONST se mantiene sin ningún cambio*/
+
+  /*STRING: cadena de texto (siempre lleva comillas "")
+  NUMBER: númro
+  NULL: nulo
+  UNDEFINED: no está asignado o definido*/
+
+  let nombre = "Daniel";
+  let invitadxsExtras = 2;
+  let esMAyorEdad = true;
+  let edad = 24;
+  let invitadeEspecial = null;
+  let horaDeSalida = undefined;
+  //CAJITAS DE PAPELERÍAS CON DATOS 
