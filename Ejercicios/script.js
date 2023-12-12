ffc6318d5d9b608d45b55139cef95d84617610e1
CeciGav <gavia.chavez.cecilia@gmail.com>
2023-12-12 11:01:17 -0600

    me quedé en el 4

---
+++ Ejercicios/script.js
@@ -0,0 +1,49 @@
+function convertir(minutos) {
+    return minutos * 60;
+  }
+  document.write(convertir(5));
+  document.write("<br>");  
+  document.write(convertir(3));
+  document.write("<br>");  
+  document.write(convertir(2));
+  document.write("<br>"); 
+  document.write("<br>");  
+
+
+function adición(number) {
+    var incrementarNumero = number + 1;
+    return incrementarNumero;
+  }
+  document.write(adición(0));
+  document.write("<br>"); 
+  document.write(adición(9));
+  document.write("<br>");  
+  document.write(adición(-3));
+  document.write("<br>");
+  document.write("<br>");  
+
+
+function tArea(ancho, alto) {
+    var area = (ancho * alto) / 2;
+    return area;
+  }
+  document.write(tArea(3, 2));
+  document.write("<br>"); 
+  document.write(tArea(7, 4));
+  document.write("<br>");  
+  document.write(tArea(10, 10));
+  document.write("<br>"); 
+  document.write("<br>"); 
+  
+
+  function calcAge(ageYears) {
+    var ageDays = ageYears * 365;
+    return ageDays;
+  }
+  document.write(calcAge(65));
+  document.write("<br>"); 
+  document.write(calcAge(0));
+  document.write("<br>");  
+  document.write(calcAge(20));
+  document.write("<br>");
+  document.write("<br>");
